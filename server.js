a0f4b9349b3ecd395361d785963803dca2352a3c
Arghadeep Ghosh <arghadeepghosh73@gmail.com>
2021-01-28 19:50:19 +0530

    'auth'

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const port = process.env.port || 3300
 
 // Assets
 app.use(express.static('public'))
-app.get('/', (req,res) => {
-    res.render('home')
-})
+
+
 
 // set Template engine
 
@@ -17,7 +16,24 @@ app.use(expressLayout)
 app.set('views',path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+
+app.get('/', (req,res) => {
+    res.render('home')
+}) 
+
+app.get('/cart',(req, res) => {
+    res.render('customers/cart')
+})
+
+app.get('/login',(req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register',(req, res) => {
+    res.render('auth/register')
+})
+
+
 app.listen(port , () => {
     console.log(`Listening on port ${port}`)
 })
-   
