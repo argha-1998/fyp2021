48abafdbe8a582ce46d330bb997d09e38a9edfba
argha-1998 <48983162+argha-1998@users.noreply.github.com>
2021-04-12 23:27:32 +0530

    commit

--- server.js
+++ server.js
@@ -1,14 +1,56 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const port = process.env.port || 3300
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const { MongoStore } = require('connect-mongo')
+const MongoDbStore = require('connect-mongo')(session)
+
+// Database connection
+
+const url = 'mongodb+srv://Argha08:argha@1997@food.b9qox.mongodb.net/test?authSource=admin&replicaSet=atlas-ie7zmi-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true/food';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection failed...')
+});
+//Session store
+
+let mongoStore = new MongoDbStore({
+                 mongooseConnection: connection,
+                collection: 'sessions'
+})
+
+//Session config
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000*60*60*24} //24 hours
+    //cookie: { maxAge: 1000*15}
+}))
+
+app.use(flash())
 
 // Assets
 app.use(express.static('public'))
+app.use(express.json())
 
+// Global middleware
 
+app.use((req, res, next) => {
+    res.locals.session = req.session
+    next()
+})
 
 // set Template engine
 
@@ -17,7 +59,9 @@ app.set('views',path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 
-app.get('/', (req,res) => {
+require('./routes/web')(app)
+
+/*app.get('/', (req,res) => {
     res.render('home')
 }) 
 
@@ -31,8 +75,8 @@ app.get('/login',(req, res) => {
 
 app.get('/register',(req, res) => {
     res.render('auth/register')
-})
-
+}) */
+  
 
 app.listen(port , () => {
     console.log(`Listening on port ${port}`)
